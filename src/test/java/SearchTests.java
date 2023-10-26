a0b1b125bf7380f96c8260067cb8f2c5838a58f4
nora <nora7373@mail.ru>
2023-10-26 21:19:24 +0300

    added  commit 1

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -17,6 +17,7 @@ public class SearchTests {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+        // comment 1
     }
 
 }
